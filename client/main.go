da3810f11726b53ab229b5d3db9b0f4fada6b093
Enrique Juan de Dios <juandedioz@gmail.com>
2020-03-24 17:51:55 -0500

    [Enhancement] Refactor Response

--- client/main.go
+++ client/main.go
@@ -21,45 +21,24 @@ func main() {
 
 	g := gin.Default()
 	g.GET("/add/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
+		a := getParam(ctx, "a")
+		b := getParam(ctx, "b")
+		req := &proto.Request{A: a, B: b}
 
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-
-		req := &proto.Request{A: int64(a), B: int64(b)}
 		if response, err := client.Add(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
+			success(ctx, response)
 		} else {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
 	})
 
 	g.GET("/mult/:a/:b", func(ctx *gin.Context) {
-		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
-			return
-		}
-		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
-			return
-		}
-		req := &proto.Request{A: int64(a), B: int64(b)}
+		a := getParam(ctx, "a")
+		b := getParam(ctx, "b")
+		req := &proto.Request{A: a, B: b}
 
 		if response, err := client.Multiply(ctx, req); err == nil {
-			ctx.JSON(http.StatusOK, gin.H{
-				"result": fmt.Sprint(response.Result),
-			})
+			success(ctx, response)
 		} else {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
@@ -69,4 +48,20 @@ func main() {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 
+}
+
+func getParam(ctx *gin.Context, param string) int64 {
+	fmt.Println("getParameters...")
+	a, err := strconv.ParseUint(ctx.Param(param), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+		return 0
+	}
+	return int64(a)
+}
+
+func success(ctx *gin.Context, response *proto.Response) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"result": fmt.Sprint(response.Result),
+	})
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module grpc_tutorial
 go 1.13
 
 require (
-	github.com/gin-gonic/gin v1.6.1 // indirect
+	github.com/gin-gonic/gin v1.6.1
 	github.com/golang/protobuf v1.3.3
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/grpc v1.28.0
